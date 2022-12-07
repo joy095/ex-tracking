0ba44f699ca2e7f10c4d406871cdb4a646056677
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2022-12-07 21:31:11 +0530

    register done

--- server/routes/AuthApi.js
+++ server/routes/AuthApi.js
@@ -1,8 +1,32 @@
 import { Router } from "express";
+import bcrypt from "bcrypt";
+import User from "../models/User.js";
 const router = Router();
 
-router.post("/register", (req, res) => {
-  res.json({ message: "user is created" });
+router.post("/register", async (req, res) => {
+  // get all from data
+  const { email, password, firstName, lastName } = req.body;
+  // check if the user exists with the same email
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(406).json({ message: "User already exists." });
+    return;
+  }
+
+  const saltRounds = 10;
+  const salt = await bcrypt.genSaltSync(saltRounds);
+  const hashPassword = await bcrypt.hashSync(password, salt);
+
+  const user = await User({
+    email,
+    password: hashPassword,
+    firstName,
+    lastName,
+  });
+  await user.save();
+  // hash the epassword
+  // store user
+  res.status(201).json({ message: "user is created" });
 });
 
 export default router;
