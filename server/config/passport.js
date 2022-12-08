88f5ec285d138ee99c3ad015a90e0512fa131561
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2022-12-08 23:51:07 +0530

    update something

--- server/config/passport.js
+++ server/config/passport.js
@@ -11,7 +11,7 @@ opts.secretOrKey = "some secret.";
 export default (passport) => {
   passport.use(
     new JwtStrategy(opts, function (jwt_payload, done) {
-      User.findOne({ id: jwt_payload.sub }, function (err, user) {
+      User.findById({ id: jwt_payload._id }, function (err, user) {
         if (err) {
           return done(err, false);
         }
