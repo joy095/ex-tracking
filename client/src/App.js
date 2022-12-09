17403febd95121357cfecc43fed44ef2d6951096
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2022-12-09 20:25:12 +0530

    redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import { Outlet } from "react-router-dom";
 import ButtonAppBar from "./components/AppBar";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getUser } from "./store/auth.js";
 
 function App() {
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, []);
+
+  console.log(auth);
   return (
     <div>
       <ButtonAppBar />
